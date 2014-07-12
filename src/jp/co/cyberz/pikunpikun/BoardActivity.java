7b8767cc5699ac6ddb89a46b143854cfcb1184c8
Akira Torii <torii@cyber-z.co.jp>
2014-07-12 14:33:28 +0900

    Boardのアクティビティ名の変更

--- src/jp/co/cyberz/pikunpikun/BoardActivity.java
+++ src/jp/co/cyberz/pikunpikun/BoardActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
-public class ListActivity extends Activity {
+public class BoardActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
