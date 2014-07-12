0e5746390f9e00a4f07b57b05e1378c32f7ade3b
Akira Torii <torii@cyber-z.co.jp>
2014-07-12 14:51:07 +0900

    ボードレイアウトの生成

--- src/jp/co/cyberz/pikunpikun/BoardActivity.java
+++ src/jp/co/cyberz/pikunpikun/BoardActivity.java
@@ -14,14 +14,7 @@ public class BoardActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		/*
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-		*/
+		setContentView(R.layout.activity_board);
 	}
 
 	@Override
